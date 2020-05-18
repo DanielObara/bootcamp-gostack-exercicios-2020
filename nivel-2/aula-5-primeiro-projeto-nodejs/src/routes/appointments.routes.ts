e1538114964c4bcbab107457c53d4290f18aef47
Daniel Tsutomu Obara <daniel.tsutomu@hotmail.com>
2020-05-17 23:02:14 -0300

    Validate date

--- nivel-2/aula-5-primeiro-projeto-nodejs/src/routes/appointments.routes.ts
+++ nivel-2/aula-5-primeiro-projeto-nodejs/src/routes/appointments.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(404)
+      .json({ message: 'this hour cannot be schedule' });
+  }
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
