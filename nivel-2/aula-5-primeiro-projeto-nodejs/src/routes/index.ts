089b1ac11e0be53a570c8c4a9eb6aa646914cbae
Daniel Tsutomu Obara <daniel.tsutomu@hotmail.com>
2020-05-17 21:37:25 -0300

    config prettier and eslint

--- nivel-2/aula-5-primeiro-projeto-nodejs/src/routes/index.ts
+++ nivel-2/aula-5-primeiro-projeto-nodejs/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello teste' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'Hello teswwte' }),
+);
 
 export default routes;
