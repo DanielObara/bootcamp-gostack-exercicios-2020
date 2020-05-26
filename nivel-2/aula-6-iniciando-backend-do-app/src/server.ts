28eb98f429425ff83261dfdfd4d8ded0898feb95
Daniel Tsutomu Obara <daniel.tsutomu@hotmail.com>
2020-05-25 23:00:19 -0300

    config TypeORM

---
+++ nivel-2/aula-6-iniciando-backend-do-app/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('port 3333!');
+});
