7c70db50896502833ea54873ee0c5cbcff2f44c3
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-06-18 21:39:12 -0300

    bcrypt configuration

--- nivel-2/aula-6-iniciando-backend-do-app/src/services/CreateAppointmentService.ts
+++ nivel-2/aula-6-iniciando-backend-do-app/src/services/CreateAppointmentService.ts
@@ -2,12 +2,10 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
 interface Request {
   date: Date;
   provider_id: string;
 }
-
 class CreateAppointmentService {
   public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
