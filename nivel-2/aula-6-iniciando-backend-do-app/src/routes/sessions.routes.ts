c5e1075149f10786aa6e5bcf957c5945a59464e3
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-06-18 22:34:32 -0300

    AuthenticateUserService

---
+++ nivel-2/aula-6-iniciando-backend-do-app/src/routes/sessions.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUser.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+export default sessionsRouter;
---
+++ nivel-2/aula-6-iniciando-backend-do-app/src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMathed = await compare(password, user.password);
+
+    if (!passwordMathed) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, 'teste', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
