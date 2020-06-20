b3403999af0292fd8864d1620bceca4778926289
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-06-19 23:46:21 -0300

    Upload file with multer

---
+++ nivel-2/aula-6-iniciando-backend-do-app/src/config/upload.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
--- nivel-2/aula-6-iniciando-backend-do-app/src/services/CreateAppointmentService.ts
+++ nivel-2/aula-6-iniciando-backend-do-app/src/services/CreateAppointmentService.ts
@@ -2,6 +2,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
 interface Request {
   date: Date;
   provider_id: string;
