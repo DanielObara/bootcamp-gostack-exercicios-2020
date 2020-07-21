6f2ce2f38e6e22260b9f7f4f8e79baaf1adb27a4
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-07-20 21:01:36 -0300

    remove js transpiled files inside src

--- nivel-1/aula-4-conceitos-typescript/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var routes_1 = require("./routes");
-var app = express_1["default"]();
-app.get("/", routes_1.helloWorld);
-app.listen(3333);
