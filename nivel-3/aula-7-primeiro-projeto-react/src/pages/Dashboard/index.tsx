bc7aba71902081db9550f1f94f62853d01a8800d
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-07-28 21:45:07 -0300

    Initial project

---
+++ nivel-3/aula-7-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+	return (
+		<div>
+			<h1>dash</h1>
+		</div>
+	);
+}
+
+export default Dashboard;
---
+++ nivel-3/aula-7-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repository: React.FC = () => {
+	return (
+		<div>
+			<h1>Repo</h1>
+		</div>
+	);
+}
+
+export default Repository;
