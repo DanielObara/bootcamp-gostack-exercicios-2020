29bc7a71b7895d0bb276c3d17df87300c7748460
Daniel Obara <daniel.tsutomu@hotmail.com>
2020-08-01 20:39:14 -0300

    update package

--- nivel-3/aula-7-primeiro-projeto-react/src/App.tsx
+++ nivel-3/aula-7-primeiro-projeto-react/src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-   <h1>Hello</h1>
-  );
+	return <h1>Hello</h1>;
 }
 
 export default App;
--- nivel-3/aula-7-primeiro-projeto-react/src/routes/index.tsx
+++ nivel-3/aula-7-primeiro-projeto-react/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
